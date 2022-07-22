2fcb7364f2af0b628f3d035fe20471d1c52eb4ab
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2022-07-22 19:55:00 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./pages/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <h2>Hallo Saya Dari Navbar</h2>
+        </>
+    );
+}
+
+export default Navbar;
