adb13d50a19d360112f6210e96107dc559bff055
FathHaq <60284617+FathHaq@users.noreply.github.com>
2022-07-23 23:32:09 +0700

    + Images + Navbar 50% + Hero 50% + Instal React Router

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
+import logo from '../img/logo.png'
+
+const navs = [
+    'Home', 'About', 'Academic', 'Event', 'PMB', 'Contact Us'
+]
 
 function Navbar() {
     return (
-        <div class="container mx-auto px-8">
-            <div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">IDN Campus</a>
-  </div>
-  <div class="flex-none">
-    <ul class="menu menu-horizontal p-0">
-      <li><a>Home</a></li>
-      <li><a>About</a></li>
-      <li><a>Academic</a></li>
-      <li><a>Event</a></li>
-      <li><a>PMB</a></li>
-      <li><a>Contact Us</a></li>
-    </ul>
-  </div>
-</div>
+        <div className="container mx-auto absolute inset-x-px z-50 px-8">
+            <div className="navbar">
+                <div className="flex-1">
+                    <img src={logo} alt="" class="w-40"/>
+                </div>
+                <div class="tabs mr-12 font-sans" >
+                    {
+                        navs.map((nav, index) => {
+                            return (
+                                <a className="tab hover:font-medium text-blue-400" key={index}> {nav} </a> 
+                            )
+                        })
+                    }
+
+                    {/* {
+                        navs.map((nav, index) => {
+                            return (
+                                <li key={index} className="menu-item">
+                                    <NavLink to={`/${nav}`} className="tab hover:font-medium text-blue-400"> {nav} </NavLink> 
+                                </li>
+                            )
+                        })
+                    } */}
+
+                    {/* <a class="tab font-bold text-blue-800 border-blue-100 tab-bordered tab-active ">Home</a> 
+                    <a class="tab hover:font-medium text-blue-400">About</a> 
+                    <a class="tab hover:font-medium text-blue-400">Academic</a>
+                    <a class="tab hover:font-medium text-blue-400">Event</a>
+                    <a class="tab hover:font-medium text-blue-400">PMB</a>
+                    <a class="tab hover:font-medium text-blue-400">Contact Us</a> */}
+                    
+                </div>
+                <div>
+                    <a className="btn btn-info rounded-full text-white px-10 normal-case hover:btn-hoverBlue ">Login</a>
+                </div>
+            </div>
         </div>
     );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,33 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
+
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'idn-background' : "url('/src/img/bg1.jpeg')",
+      }
+    },
+    fontFamily: {
+      'montserrat': []
+    },
   },
-  plugins: [require("daisyui")],
+
+  plugins: [
+
+    require("daisyui"),
+
+    plugin(function({ addComponents }) {
+      addComponents({
+        '.btn-hoverBlue': {
+          backgroundColor: '#0284c7',
+        },
+      })
+    })
+
+  ],
 }
